3e96b778581ce031a2b0c86e925db483765c2bff
Jarrod Servilla <jarrod.servilla@gmail.com>
2021-06-17 11:37:00 -0400

    indicators

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,42 @@
-import Header from "../components/Header";
-import SideNav from "../components/SideNav";
+import Header from '../components/Header';
+import Indicators from '../components/Indicators';
+import SideNav from '../components/SideNav';
+
+const indicators = [
+	{
+		label: 'Total PnL',
+		number: '$1221.43',
+		color: "green.500",
+		type: 'increase',
+		percentage: '8.36%'
+	},
+	{
+		label: 'Daily PnL',
+		number: '$81.43',
+		color: "green.500",
+		type: 'increase',
+		percentage: '4.65%'
+	},
+	{
+		label: 'Risk Tolerance',
+		color: "orange.500",
+		number: '65%',
+	},
+	{
+		label: 'Sent',
+		number: '345,670',
+		type: 'increase',
+		percentage: '23.36%'
+	}
+];
 
 function App() {
 	return (
-		<div className="row" style={{ width: "100vw" }}>
+		<div className="row" style={{ width: '100vw' }}>
 			<SideNav />
 			<div className="col">
 				<Header />
+				<Indicators indicators={indicators} />
 			</div>
 		</div>
 	);
